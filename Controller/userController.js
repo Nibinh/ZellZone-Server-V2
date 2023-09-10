b2cbb1951d30e5acfae8b8b807f8f5190ec312ef
Nibinh <haridasnibin@gmail.com>
2023-09-10 22:30:39 +0530

    edit

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,14 +1,17 @@
 const User = require("../Models/users");
 const bucketName = process.env.BUCKET_NAME;
-const { GetObjectCommand } = require("@aws-sdk/client-s3");
+const { GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const { s3Client } = require("../Services/AWS-S3");
+const bcrypt = require("bcryptjs");
 
+//getting a particular one
 const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const data = await User.findOne({ _id: id }).select("-password");
     if (!data) return res.status(400).send("User not found");
+    // aws
     const param = {
       Bucket: bucketName,
       Key: data.imageName,
@@ -22,6 +25,74 @@ const getUser = async (req, res) => {
   }
 };
 
+//getting all users
+const gettingAllUsers = async (req, res) => {
+  try {
+    const data = await User.find().select("-password");
+    if (!data) res.status(400).send("No users");
+    for (let post of data) {
+      const param = {
+        Bucket: bucketName,
+        Key: post.imageName,
+      };
+      const command = new GetObjectCommand(param);
+      const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+      post.imageUrl = url;
+    }
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//edit user
+const editUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, password, confirmPassword, phonenumber, address } = req.body;
+    const newImageName = req.file.key;
+
+    const isExisting = await User.findOne({ _id: id });
+    if (!isExisting) return res.status(400).send("User not found");
+
+    if (password !== confirmPassword)
+      return res.status(400).send("password and ConfirmPassword dosent match");
+    //Hashing password.
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    //deleting image if image is updated
+    if (newImageName) {
+      const deleteParam = {
+        Bucket: bucketName,
+        Key: isExisting.imageName,
+      };
+      await s3Client.send(new DeleteObjectCommand(deleteParam));
+      const updatedData = await User.updateOne(
+        { _id: id },
+        {
+          $set: {
+            name,
+            imageName: newImageName,
+            password: hashPassword,
+            phonenumber,
+            address,
+          },
+        }
+      );
+      res.status(200).send(updatedData);
+    } else {
+      const updatedData = await User.updateOne(
+        { _id: id },
+        { $set: { name, password: hashPassword, phonenumber, address } }
+      );
+      res.status(200).send(updatedData);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 module.exports = {
   getUser,
+  gettingAllUsers,
+  editUser,
 };
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../Services/middleware");
+const { upload } = require("../Services/AWS-S3");
 
 const user = require("../Controller/userController");
 
 router.get("/getUser/:id", verifyToken, user.getUser);
+router.get("/getallusers", user.gettingAllUsers);
+router.put("/edituser/:id", upload.single("image"), user.editUser);
 
 module.exports = router;
