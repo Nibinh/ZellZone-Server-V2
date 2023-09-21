af2be886b3cbae3e8674b0b7da0aefd4fac0965a
Nibinh <haridasnibin@gmail.com>
2023-09-21 17:32:59 +0530

    hostfive

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../Services/AWS-S3");
-
+const { verifyToken } = require("../Services/middleware");
 const auth = require("../Controller/authController");
 
 router.post("/register", upload.single("image"), auth.registerUser);
 router.post("/login", auth.loginUser);
-router.post("/logout", auth.logout);
+router.post("/logout", verifyToken, auth.logout);
 
 module.exports = router;
--- Routes/wishlistRoute.js
+++ Routes/wishlistRoute.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+const { verifyToken } = require("../Services/middleware");
 const wishlist = require("../Controller/wishlistController");
 
-router.get("/addtowishlist/:userEmail/:prodId", wishlist.addToWishlist);
-router.get("/removing/:userEmail/:prodId", wishlist.removeFromWishlist);
-router.get("/gettingall/:id", wishlist.viewAllWishlist);
+router.get(
+  "/addtowishlist/:userEmail/:prodId",
+  verifyToken,
+  wishlist.addToWishlist
+);
+router.get(
+  "/removing/:userEmail/:prodId",
+  verifyToken,
+  wishlist.removeFromWishlist
+);
+router.get("/gettingall/:id", verifyToken, wishlist.viewAllWishlist);
 
 module.exports = router;
